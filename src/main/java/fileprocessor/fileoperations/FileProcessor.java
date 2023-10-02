754c682872b4574ad42760386b25ff263bc59ca6
Disha <singhdisha@gmail.com>
2023-10-02 14:56:11 +0530

    csv will not be processed if there is no trg'

--- src/main/java/fileprocessor/fileoperations/FileProcessor.java
+++ src/main/java/fileprocessor/fileoperations/FileProcessor.java
@@ -10,117 +10,122 @@ import java.util.Scanner;
 
 public class FileProcessor {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileProcessor.class);
+	private static final Logger logger = LoggerFactory.getLogger(FileProcessor.class);
 
-    public static void main(String[] args) {
-        String sourceFolder = "C:\\Users\\DishaSingh\\Documents\\source";
-        String destinationFolder = "C:\\Users\\DishaSingh\\Documents\\destination";
+	public static void main(String[] args) {
+		String sourceFolder = "C:\\Users\\DishaSingh\\Documents\\source";
+		String destinationFolder = "C:\\Users\\DishaSingh\\Documents\\destination";
 
-        try {
-            processFiles(sourceFolder, destinationFolder);
-        } catch (FileNotFoundException e) {
-            logger.error("Error processing files: {}", e.getMessage());
-        } catch (SourceFolderDoesNotExistException e) {
-            logger.error("Source folder does not exist or is empty: {}", e.getMessage());
-        }
-    }
+		try {
+			processFiles(sourceFolder, destinationFolder);
+		} catch (FileNotFoundException e) {
+			logger.error("Error processing files: {}", e.getMessage());
+		} catch (SourceFolderDoesNotExistException e) {
+			logger.error("Source folder does not exist or is empty: {}", e.getMessage());
+		}
+	}
 
-    private static void processFiles(String sourceFolder, String destinationFolder)
-            throws FileNotFoundException, SourceFolderDoesNotExistException {
-        File sourceDir = new File(sourceFolder);
-        File[] files = sourceDir.listFiles();
+	private static void processFiles(String sourceFolder, String destinationFolder)
+			throws FileNotFoundException, SourceFolderDoesNotExistException {
+		File sourceDir = new File(sourceFolder);
+		File[] files = sourceDir.listFiles();
 
-        if (files != null) {
-            boolean csvOrTxtFileFound = false;
+		if (files != null) {
+			boolean csvOrTxtFileFound = false;
 
-            for (File file : files) {
-                if (file.getName().endsWith(".trg")) {
-                    file.delete();
-                } else if (file.getName().endsWith(".csv")) {
-                    csvOrTxtFileFound = true;
-                    processCsvFile(file, destinationFolder);
-                } else if (file.getName().matches(".*\\.(txt|other_extension)")) {
-                    csvOrTxtFileFound = true;
-                    processDataFile(file, destinationFolder);
-                }
-            }
+			for (File file : files) {
+				if (file.getName().endsWith(".trg")) {
+					String baseFileName = file.getName().replace(".trg", "");
+					File csvFile = new File(sourceFolder, baseFileName + ".csv");
+					File txtFile = new File(sourceFolder, baseFileName + ".txt");
 
-            if (!csvOrTxtFileFound) {
-                throw new FileNotFoundException("No .csv or .txt file found in the source folder.");
-            }
-        } else {
-            throw new SourceFolderDoesNotExistException("Source folder does not exist or is empty.");
-        }
-    }
+					if (csvFile.exists()) {
+						csvOrTxtFileFound = true;
+						processCsvFile(csvFile, destinationFolder);
+					} else if (txtFile.exists()) {
+						csvOrTxtFileFound = true;
+						processDataFile(txtFile, destinationFolder);
+					}
+				}
+			}
 
-    private static void processCsvFile(File csvFile, String destinationFolder) {
-        try (Scanner scanner = new Scanner(csvFile)) {
-            if (scanner.hasNextLine()) {
-                scanner.nextLine();
-            }
+			if (!csvOrTxtFileFound) {
+				throw new FileNotFoundException(
+						"No .csv or .txt file found for existing .trg files in the source folder.");
+			}
+		} else {
+			throw new SourceFolderDoesNotExistException("Source folder does not exist or is empty.");
+		}
+	}
 
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(",");
+	private static void processCsvFile(File csvFile, String destinationFolder) {
+		try (Scanner scanner = new Scanner(csvFile)) {
+			if (scanner.hasNextLine()) {
+				scanner.nextLine();
+			}
 
-                if (parts.length == 2) {
-                    String folder = parts[0].trim();
-                    boolean createRevision = Boolean.parseBoolean(parts[1].trim());
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] parts = line.split(",");
 
-                    logger.info("Processing data files for folder: {}", folder);
+				if (parts.length == 2) {
+					String folder = parts[0].trim();
+					boolean createRevision = Boolean.parseBoolean(parts[1].trim());
 
-                    if (createRevision) {
-                        logger.info("Creating revisions for files in folder: {}", folder);
-                    } else {
-                        logger.info("Copying files to folder: {}", folder);
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            logger.error("CSV file not found: {}", e.getMessage());
-        }
-    }
+					logger.info("Processing data files for folder: {}", folder);
 
-    private static void processDataFile(File dataFile, String destinationFolder) {
-        String sourceFileName = dataFile.getName();
-        String destinationFileName = sourceFileName;
+					if (createRevision) {
+						logger.info("Creating revisions for files in folder: {}", folder);
+					} else {
+						logger.info("Copying files to folder: {}", folder);
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			logger.error("CSV file not found: {}", e.getMessage());
+		}
+	}
 
-        if (sourceFileName.matches(".*\\.(txt|other_extension)")) {
-            int revision = 1;
-            File destinationFile = new File(destinationFolder, destinationFileName);
-            while (destinationFile.exists()) {
-                revision++;
-                destinationFileName = sourceFileName.replaceFirst("$", "(" + revision + ").$0");
-                destinationFile = new File(destinationFolder, destinationFileName);
-            }
+	private static void processDataFile(File dataFile, String destinationFolder) {
+		String sourceFileName = dataFile.getName();
+		String destinationFileName = sourceFileName;
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(dataFile));
-                 FileWriter writer = new FileWriter(destinationFile)) {
+		if (sourceFileName.matches(".*\\.(txt|other_extension)")) {
+			int revision = 1;
+			File destinationFile = new File(destinationFolder, destinationFileName);
+			while (destinationFile.exists()) {
+				revision++;
+				destinationFileName = sourceFileName.replaceFirst("$", "(" + revision + ").$0");
+				destinationFile = new File(destinationFolder, destinationFileName);
+			}
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    writer.write(line);
-                    writer.write("\n");
-                }
+			try (BufferedReader reader = new BufferedReader(new FileReader(dataFile));
+					FileWriter writer = new FileWriter(destinationFile)) {
 
-                logger.info("Copied and processed data file: {}", sourceFileName);
-            } catch (IOException e) {
-                logger.error("Error processing data file: {}", e.getMessage());
-                writeErrorToFile(e.getMessage(), destinationFolder, sourceFileName);
-            }
-        } else {
-            logger.info("Skipping unsupported data file: {}", sourceFileName);
-        }
-    }
+				String line;
+				while ((line = reader.readLine()) != null) {
+					writer.write(line);
+					writer.write("\n");
+				}
 
-    private static void writeErrorToFile(String errorMessage, String destinationFolder, String fileName) {
-        File errFile = new File(destinationFolder, fileName.replaceFirst("[.][^.]+$", ".err"));
+				logger.info("Copied and processed data file: {}", sourceFileName);
+			} catch (IOException e) {
+				logger.error("Error processing data file: {}", e.getMessage());
+				writeErrorToFile(e.getMessage(), destinationFolder, sourceFileName);
+			}
+		} else {
+			logger.info("Skipping unsupported data file: {}", sourceFileName);
+		}
+	}
 
-        try (FileWriter writer = new FileWriter(errFile, true)) {
-            writer.write(errorMessage);
-            writer.write("\n");
-        } catch (IOException e) {
-            logger.error("Error writing to error file: {}", e.getMessage());
-        }
-    }
+	private static void writeErrorToFile(String errorMessage, String destinationFolder, String fileName) {
+		File errFile = new File(destinationFolder, fileName.replaceFirst("[.][^.]+$", ".err"));
+
+		try (FileWriter writer = new FileWriter(errFile, true)) {
+			writer.write(errorMessage);
+			writer.write("\n");
+		} catch (IOException e) {
+			logger.error("Error writing to error file: {}", e.getMessage());
+		}
+	}
 }
